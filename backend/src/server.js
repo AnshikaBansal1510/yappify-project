37e2212ffe378db1c43ff20d3de73d9ccfe07917
Anshika Bansal <anshikabansal1618@gmail.com>
2025-12-09 18:20:04 +0530

    add initial files for setup

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv"
+
+import authRoutes from "./routes/auth.route.js"
+
+// import "dotenv/config"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+})
