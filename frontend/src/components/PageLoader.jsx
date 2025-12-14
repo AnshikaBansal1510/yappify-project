f5aa95725fc4a0e4aa68e8f10269c6e6a21d9c78
Anshika Bansal <anshikabansal1618@gmail.com>
2025-12-14 15:04:39 +0530

    navbar changed and added logout functionality

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -1,8 +1,6 @@
 import { LoaderIcon } from "lucide-react";
-//import { useThemeStore } from "../store/useThemeStore";
 
 const PageLoader = () => {
-  //const { theme } = useThemeStore();
   return (
     <div className="min-h-screen flex items-center justify-center">
       <LoaderIcon className="animate-spin size-10 text-primary" />
