21a4734c2be8e4af090a5aa93cbcfdcad7f8d33f
Anshika Bansal <anshikabansal1618@gmail.com>
2026-03-01 14:21:49 +0530

    deployment changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,8 +8,12 @@ import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
 import connectDB  from "./db/index.js";
 
+import path from "path";
+
 const app = express();
 const PORT = process.env.PORT || 5001
+
+const __dirname = path.resolve();
 
 app.use(cors({
   origin: "http://localhost:5173",
@@ -21,6 +25,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/chat", chatRoutes)
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
